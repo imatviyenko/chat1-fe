49d67953b8bd4b9c11bf14debb9a289611d8a05a
mia <ivan.matviyenko@gmail.com>
2019-12-07 19:39:28 +0300

    Initial commit in chat1-fe

---
+++ src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import References from '../References/References';
+
+function App() {
+  return (
+    <div className="chat1-app">
+        <h3>Component: App</h3>
+        <p>
+          Welcome to chats! 
+        </p>
+        <References />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/References/References.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './References.css';
+
+function References() {
+  return (
+    <div className="chat1-references">
+        <h3>Component: References</h3>
+        <ul>
+          <li>
+            <a href='http://www.iconarchive.com/show/100-flat-2-icons-by-graphicloads/chat-2-icon.html'> Free icons at IconAarchive </a>
+          </li>
+        </ul>
+    </div>
+  );
+}
+
+export default References;
